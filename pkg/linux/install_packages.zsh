90145fedc370a1d4209a621b9fe82ead4a223933
Robert Scott-Buccleuch <60840+rsb@users.noreply.github.com>
2021-06-09 09:35:27 -0700

    adding fender and rsb to workspaces

--- pkg/linux/install_packages.zsh
+++ pkg/linux/install_packages.zsh
@@ -37,5 +37,5 @@ fi
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-sudo apt-get -y install httpie exa jq bat fd-find ripgrep
+sudo apt-get -y install httpie exa jq bat fd-find ripgrep tmux
 
